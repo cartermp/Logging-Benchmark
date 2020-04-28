6ff019b5695f3cff68855d163ff60e1556f331e2
cartermp <pcarter@fastmail.com>
2020-04-28 10:39:14 -0700

    Remove the workarounds due to .tail issues

--- Logging Performance Test - Core Library - FS/Library.fs
+++ Logging Performance Test - Core Library - FS/Library.fs
@@ -5,23 +5,17 @@ open System.Runtime.CompilerServices
 
 type Logger() =
     let messages = System.Collections.Generic.List<string>(10000);
-    
-    [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
-    let nop () = ()
+
     member this.Log(level : LoggerLevel, msg : string) =
         messages.Add(msg)
-        nop()
 
     member this.Log(level : LoggerLevel, obj : 'a) =
         messages.Add(obj.ToString())
-        nop()
 
 
     interface ILogger with
         member this.Log(level : LoggerLevel, msg : string) =
             messages.Add(msg)
-            nop()
 
         member this.Log(level : LoggerLevel, obj : 'a) =
             messages.Add(obj.ToString())
-            nop()
